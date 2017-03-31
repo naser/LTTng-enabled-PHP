975af23d7255430c033e1547bb3213897cc62f07
naser <ezzati@gmail.com>
2017-03-31 14:40:05 -0400

    Update lttng.c

--- lttng.c
+++ lttng.c
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2012 Yahav Gindi Bar <g.b.yahav@gmail.com>
+ * Copyright (C) 2017 Naser Ezzati <ezzati@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
